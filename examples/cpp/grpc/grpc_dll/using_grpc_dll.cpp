e11765c038373cac3a64ec054d57bffd1006ac45
Chad Walters <cwalter@microsoft.com>
2017-05-22 16:52:05 -0700

    fixup! [C++ gRPC] Add Windows dll example

--- examples/cpp/grpc/grpc_dll/using_grpc_dll.cpp
+++ examples/cpp/grpc/grpc_dll/using_grpc_dll.cpp
@@ -134,7 +134,7 @@ int main()
 
     std::cout << schema.GetSchema().structs[schema.GetSchema().root.struct_def].fields[0].metadata.name << std::endl;
 
-    // Exersize gRPC facilities
+    // Exercize gRPC facilities
 
     const std::string server_address("127.0.0.1:50051");
 
