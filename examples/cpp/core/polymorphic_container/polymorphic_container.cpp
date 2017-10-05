a14486f0d3fd70cb0dd0de3922a492db98fa32cc
Christopher Warrington <chwarr@microsoft.com>
2017-10-05 15:37:38 -0700

    EXAMPLE: Buggy behavior when deserializing something with a base and encountering BT_STOP during the base instead of BT_STOP_BASE

--- examples/cpp/core/polymorphic_container/polymorphic_container.cpp
+++ examples/cpp/core/polymorphic_container/polymorphic_container.cpp
@@ -18,91 +18,42 @@ bond::bonded<T> Serialize(const T& obj)
     return bond::bonded<T>(reader);
 }
 
-
-Struct1 DeserializeStruct1(const bond::bonded<Base>& base)
-{
-    // Explicit down-casting from bonded<Base> to bonded<Struct1>
-    bond::bonded<Struct1> bonded(base);
-    Struct1 obj;
-    bonded.Deserialize(obj);
-    return obj;
-}
-
-
-Struct2 DeserializeStruct2(const bond::bonded<Base>& base)
-{
-    // Explicit down-casting from bonded<Base> to bonded<Struct2>
-    bond::bonded<Struct2> bonded(base);
-    Struct2 obj;
-    bonded.Deserialize(obj);
-    return obj;
-}
-
-
 int main()
 {
-    bond::bonded<Polymorphic> bonded;
+    bond::bonded<bond::Box<std::vector<bond::bonded<Base>>>> bonded;
 
     {
-        Struct1 obj1;
-        obj1.kind = StructKind_Struct1;
-        obj1.n = 1;
-        obj1.str = "1";
+        bond::Box<std::vector<bond::bonded<Base>>> box;
 
-        Struct2 obj2;
-        obj2.kind = StructKind_Struct2;
-        obj2.n = 2;
-        obj2.str = "2";
+        {
+            Base b1, b2;
+            b1.f = 1;
+            b2.f = 2;
 
-        Polymorphic polymorphic;
+            box.value.emplace_back(Serialize(b1));
+            box.value.emplace_back(Serialize(b2));
+        }
 
-        // Implicit up-casting from bonded<Struct1/2> to bonded<Base>
-        polymorphic.items.push_back(Serialize(obj1));
-        polymorphic.items.push_back(Serialize(obj2));
-
-        bonded = Serialize(polymorphic);
+        bonded = Serialize(box);
     }
 
-    // At this point bonded represents serialized data for a struct which has
-    // a polymorpic list field delcared as list<bonded<Base>>. Application can
-    // deserialize the Base part of each element and based of the contained type
-    // metadata, deserialize the full element to an appropriate struct.
-
-    Polymorphic polymorphic;
-
-    bonded.Deserialize(polymorphic);
-
-    for (
-        std::list<bond::bonded<Base> >::const_iterator it = polymorphic.items.begin();
-        it != polymorphic.items.end();
-        ++it)
     {
-        Base base;
-        it->Deserialize(base);
+        bond::Box<std::vector<bond::bonded<Base>>> obj1;
+        bonded.Deserialize(obj1);
 
-        switch (base.kind)
-        {
-            case StructKind_Struct1:
-            {
-                Struct1 obj1 = DeserializeStruct1(*it);
-                assert(obj1.str == "1");
-                assert(obj1.n == 1);
-            }
-            break;
+        assert(obj1.value.size() == 2);
 
-            case StructKind_Struct2:
-            {
-                Struct2 obj2 = DeserializeStruct2(*it);
-                assert(obj2.b == false);
-                assert(obj2.n == 2);
-                assert(obj2.str == "2");
-            }
-            break;
+        bond::bonded<Derived> bondedD(obj1.value[0]);
+        Derived d;
+        bondedD.Deserialize(d);
 
-            default:
-                assert(false);
-                break;
-        }
+        assert(d.f == 1);
+        // the default value for df is 0, so we sort of "expect" that.
+        // Really, though, I think that deserialization should fail at
+        // runtime. There could be an argument made for deserialization
+        // working if there are no required fields in any of the child
+        // parts...
+        assert(d.df == 0);
     }
 
     return 0;
